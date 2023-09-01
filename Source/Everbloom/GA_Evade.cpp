4c9b1a3112bded26b479d395c5838163dd3b3023
ArcherPharis <62902662+ArcherPharis@users.noreply.github.com>
2023-09-01 13:22:20 -0500

    evasion implemented

--- Source/Everbloom/GA_Evade.cpp
+++ Source/Everbloom/GA_Evade.cpp
@@ -3,27 +3,16 @@
 
 #include "GA_Evade.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
 #include "BaseCharacter.h"
 #include "GameFramework./CharacterMovementComponent.h"
+
 
 
 
 void UGA_Evade::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	//BrakingDeccelerationWalking and GroundFriction to 0
-	ACharacter* Chara = Cast<ACharacter>(GetAvatarActorFromActorInfo());
-	UCharacterMovementComponent* MoveComp = Chara->GetCharacterMovement();
-	if (MoveComp->IsFalling())
-	{
-		//K2_EndAbility();
-		//return;
-	}
-	OriginalBrakingWalkingSpeed = MoveComp->BrakingDecelerationWalking;
-	OriginalGroundFriction = MoveComp->GroundFriction;
-
-	MoveComp->BrakingDecelerationWalking = 0;
-	MoveComp->GroundFriction = 0;
-	MoveComp->AddImpulse(Chara->GetActorForwardVector() * ForwardForce, false);
 
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	UAbilityTask_PlayMontageAndWait* EvadeMontagePlay = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, EvadeMontage);
@@ -36,14 +25,48 @@ void UGA_Evade::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 		EvadeMontagePlay->ReadyForActivation();
 	}
 
+	UAbilityTask_WaitGameplayEvent* EndMoveEvent = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, EndMovementTag, nullptr, false, false);
+	if (EndMoveEvent)
+	{
+		EndMoveEvent->EventReceived.AddDynamic(this, &UGA_Evade::EndMovement);
+		EndMoveEvent->ReadyForActivation();
+	}
+
+	ACharacter* Chara = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+	UCharacterMovementComponent* MoveComp = Chara->GetCharacterMovement();
+	OriginalGroundFriction = MoveComp->GroundFriction;
+	FVector EvadeDirection;
+	if (MoveComp->GetLastInputVector() == FVector::ZeroVector)
+	{
+		APlayerController* PlayerController = Cast<APlayerController>(Chara->GetController());
+		FRotator ControlRotation = PlayerController->GetControlRotation();
+		EvadeDirection = ControlRotation.Vector();
+	}
+	else
+	{
+		EvadeDirection = MoveComp->GetLastInputVector();
+	}
+	FRotator EvadeRotation = FRotator(0, EvadeDirection.Rotation().Yaw, 0);
+	MoveComp->GroundFriction = 0;
+	FVector EvadeVelocity = EvadeDirection.GetSafeNormal() * EvadeSpeed;
+	MoveComp->Velocity = EvadeVelocity;
+	Chara->SetActorRotation(EvadeRotation);
 }
 
 void UGA_Evade::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
+
+
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 	ACharacter* Chara = Cast<ACharacter>(GetAvatarActorFromActorInfo());
 	UCharacterMovementComponent* MoveComp = Chara->GetCharacterMovement();
-	MoveComp->BrakingDecelerationWalking = OriginalBrakingWalkingSpeed;
 	MoveComp->GroundFriction = OriginalGroundFriction;
+	MoveComp->StopMovementImmediately();
 
+
+}
+
+void UGA_Evade::EndMovement(FGameplayEventData Payload)
+{
+	K2_EndAbility();
 }
--- Source/Everbloom/GA_Evade.h
+++ Source/Everbloom/GA_Evade.h
@@ -18,13 +18,22 @@ private:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
 	
+	UFUNCTION()
+	void EndMovement(FGameplayEventData Payload);
+
 	UPROPERTY(EditDefaultsOnly, Category = "Evade")
 	UAnimMontage* EvadeMontage;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Evade")
+	FGameplayTag EndMovementTag;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Evade")
 	float ForwardForce = 600.f;
 
-	float OriginalBrakingWalkingSpeed;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Evade")
+	float EvadeSpeed = 600.f;
+
 	float OriginalGroundFriction;
 
 
